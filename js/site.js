bc505189925de884f910a51339ddc6d021411b87
EB03 <35344980+EB03@users.noreply.github.com>
2021-09-14 12:10:42 -0400

    working on script

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
-// Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+
+
+function calcMonthlyPayments() {
+    let loanAmount = document.getElementById("loanAmount");
+    let term = document.getElementById("term");
+    let interestRate = document.getElementById("interestRate");
+
+    let monthlyPayments = document.getElementById("monthlyPayments");
+
+    monthlyPayments = (loanAmount) * (interestRate/1200) / (1-(1 + interestRate/1200)^(-term));
 }
